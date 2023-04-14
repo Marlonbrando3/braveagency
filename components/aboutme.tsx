94c12f7779e510d7f43d96c33b25bac545566677
Marek <marek@macbook-air-marek.home>
2023-04-14 07:08:07 +0200

    first commit

--- components/aboutme.tsx
+++ components/aboutme.tsx
@@ -7,7 +7,7 @@ export default function Aboutme() {
       <div className='lg:w-[1140px] mx-auto lg:px-0 px-3'>
           <div className='my-20'>
               <span className='bg-[#423e59] text-3xl py-2 px-5 text-white'>About me</span>
-              <p className='mt-10 text-2xl pb-20 font-[300]'>I own several brands on the differend markets. My main porpouse is to work with people around the world. I have expirience and skill in few fields. I love to sell, perfom presentations with clients, make a coding job, create and run digitaln marketing for my clinest. I&apos;m Freelancer, copywtirer, seller, coder and so on.I choosed to work with any organization remote only. </p>
+              <p className='mt-10 text-2xl pb-20 font-[300]'>I own several brands on the differend markets. My main porpouse is to work with people around the world. I have experience and skill in few fields. I love to sell, perfom presentations with clients, make a coding job, create and run digitaln marketing for my clinest. I&apos;m Freelancer, copywtirer, seller, coder and so on.I choosed to work with any organization remote only. </p>
           </div>
           </div>
     </>
