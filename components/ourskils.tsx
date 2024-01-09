ca280a1c7d610ebe70bb743c09e2c1d90747ba13
„PersonalTaxi” <personaltaxiads@gmail.com>
2024-01-09 18:50:00 +0100

    first commit

--- components/ourskils.tsx
+++ components/ourskils.tsx
@@ -8,25 +8,25 @@ export default function Ourskills() {
         <div className="mt-20 mb-4 flex items-start flex-col-reverse md:flex-auto">
           <p className="mt-4 md:text-2xl pb-20 font-[300] md:mr-[40px]">
             Mamy doświadczenie w tworzeniu szerokich, skutecznych strategi marketingowych z
-            włączeniem rozwiązań technologii (jesteśmy w stanie stworzyć dedykowane oprogrmowanie).
+            włączeniem rozwiązań technologicznych (jesteśmy w stanie stworzyć dedykowane
+            oprogramowanie).
             <br></br>
             Znaczy to, że wszelkie działania w ramach budownia świadomości marki, podnoszenia
-            sprzedaży, tworzenia procesów sprzedażowych czy też samej sprzedaży i prezentacji
+            sprzedaży, tworzenia procesów sprzedażowych czy też samej sprzedaży, prezentacji
             biznesowych były przez nas realizowane i wypracowaliśmy skuteczne metody dzięki którym
             zrealizowaliśmy wyniki sprzedażowe.
             <br></br>
             <br></br>W zależności od działań, które potencjalnie będą skuteczne w Twojej branży
             możemy zacząć od{" "}
-            <b>zaprojektowania strony zakłądającej realizowanie danego rezultatu. </b>
-            Oprzemy się o proces komunikacji zgodny z charakterm danej grupy odbiorców lub jeśli
-            będzie potrzebny proces edukacji w grupie docelowej. Reklamę płatną w <b>
-              Google Ads
-            </b>{" "}
-            lub <b>Facebook Ads</b> jeśli okażą się potrzebne. Skorzystamy z <b>mailingu</b>, jeśli
-            np. proces decyzyjny jest dłuższy lub szukamy ważniejszych / mniej dostepnych kleintów
-            lub firm.<br></br>
-            Działania stworzą <b>strategię</b> w oparciu o którą będziemy mogli mierzyć skuteczność
-            naszych działań, wprowadzać korekty - udoskonalać model sprzedażowy.
+            <b>zaprojektowania strony zakładającej realizowanie ustalonego rezultatu. </b>
+            Dalej oprzemy się o proces komunikacji zgodny z charakterm Twojej grupy odbiorców lub
+            jeśli będzie potrzebny - proces edukacji w grupie docelowej. Reklamę płatną w{" "}
+            <b>Google Ads</b> lub <b>Facebook Ads</b> ponieważ zazwyczaj są skuteczne. Skorzystamy z{" "}
+            <b>mailingu</b>, jeśli np. proces decyzyjny jest dłuższy lub szukamy ważniejszych /
+            mniej dostepnych odbiorców lub firm.<br></br>
+            Działania stworzą <b>strategię</b> w oparciu o którą będziemy mogli działać, mierzyć
+            skuteczność, wprowadzać korekty - udoskonalać model sprzedażowy i finalnie realizować
+            cele kampanii oraz Twjej firmy.
           </p>
           <span className="bg-orange-500 font-bold text-xl py-1 text-white px-[10px] rounded-l-[20px] w-[300px] hidden md:visible">
             Nasze umiejętności
